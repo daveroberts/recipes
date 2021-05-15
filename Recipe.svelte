a5b4efd735c94e87bb3310c76b556b91577a5ef6
Dave Roberts <dave.a.roberts@gmail.com>
2021-05-14 22:24:44 -0400

    WIP

--- Recipe.svelte
+++ Recipe.svelte
@@ -1,10 +1,27 @@
 <!-- Recipe.svelte -->
 <script>
   export let recipe;
+  let expanded = false
 </script>
 <style>
+  .external_link{ text-decoration: underline; }
 </style>
 <div>
-  <h2>{recipe.name}</h2>
-  <pre>{JSON.stringify(recipe,null,2)}</pre>
+  <h2><a href={'#'} on:click={()=>{expanded=!expanded}}>{recipe.name}</a></h2>
+  {#if expanded}
+    {#if recipe.note}<div><strong>Note: {recipe.note}</strong></div>{/if}
+    {#if recipe.video}<div><a class="external_link" target="_blank" href={recipe.video}>{recipe.video}</a></div>{/if}
+    <h3>Ingredients</h3>
+    <ul>
+      {#each recipe.ingredients as ingredient}
+        <li>{ingredient}</li>
+      {/each}
+    </ul>
+    <h3>Instructions</h3>
+    <ol>
+      {#each recipe.instructions as instruction}
+        <li>{instruction}</li>
+      {/each}
+    </ol>
+  {/if}
 </div>
--- RecipeList.svelte
+++ RecipeList.svelte
@@ -1,27 +1,50 @@
 <!-- RecipeList.svelte -->
 <script>
   import all_recipes from './recipes.json'
+  let categories = all_recipes.map(r=>r.categories).flat().filter((v,i,a)=>a.indexOf(v)===i).filter(cat=>!!cat)
   import Recipe from './Recipe.svelte'
   let search = ""
+  let selected_categories = []
 
   let recipes = all_recipes
   $: {
+    recipes = all_recipes.slice()
+    if (selected_categories.length > 0){
+      recipes = recipes.filter(r=>{
+        if (!r.categories){ return false }
+        let intersection = r.categories.filter(cat => selected_categories.includes(cat))
+        return intersection.length > 0
+      })
+    }
     if(search){
-      recipes = all_recipes.filter(r=>{
+      recipes = recipes.filter(r=>{
         return JSON.stringify(r).toLowerCase().includes(search)
       })
-    } else {
-      recipes = all_recipes
     }
+    recipes.sort((a,b)=>a.name > b.name ? 1 : -1)
   }
 </script>
 <style>
+  .category_area{ margin-bottom: 1em; }
+  .category_checkbox{ margin-right: 3em;}
 </style>
 <div>
+  <div class="category_area">
+    {#each categories as category}
+      <label class="category_checkbox">
+        <input type="checkbox" bind:group={selected_categories} value={category}>
+        {category}
+      </label>
+    {/each}
+  </div>
   <div>Search: <input bind:value={search} /></div>
   {#if search}<div>Searching for {search}</div>{/if}
   
-  {#each recipes as recipe}
-    <Recipe recipe={recipe}></Recipe>
-  {/each}
+  {#if recipes.length > 0}
+    {#each recipes as recipe}
+      <Recipe recipe={recipe}></Recipe>
+    {/each}
+  {:else}
+    <div>No recipes found with `{search}`</div>
+  {/if}
 </div>
