afb3782c204b4b7143ca2850547b63ab3321c6fb
Dave Roberts <dave.a.roberts@gmail.com>
2021-05-30 18:05:22 -0400

    WIP

--- src/Recipe.svelte
+++ src/Recipe.svelte
@@ -5,25 +5,35 @@
 </script>
 <style>
   .external_link{ text-decoration: underline; }
-  .fakelink:hover{ cursor: pointer }
 </style>
 <div>
   <h2><span class="fakelink" on:click={()=>{expanded=!expanded}}>{recipe.name}</span></h2>
   {#if expanded}
-    {#if recipe.note}<div><strong>Note: {recipe.note}</strong></div>{/if}
+    {#if recipe.notes}
+      {#each recipe.notes as note}
+        <div><strong>Note: {note}</strong></div>
+      {/each}
+    {/if}
     {#if recipe.video}<div><a class="external_link" target="_blank" href={recipe.video}>{recipe.video}</a></div>{/if}
     {#if recipe.link}<div><a class="external_link" target="_blank" href={recipe.link}>{recipe.link}</a></div>{/if}
-    <h3>Ingredients</h3>
-    <ul>
-      {#each recipe.ingredients as ingredient}
-        <li>{ingredient}</li>
-      {/each}
-    </ul>
-    <h3>Instructions</h3>
-    <ol>
-      {#each recipe.instructions as instruction}
-        <li>{instruction}</li>
-      {/each}
-    </ol>
+    {#each recipe.versions as version}
+      {#if version.name}<h3>{version.name}</h3>{/if}
+      {#if version.ingredients}
+        <h4>Ingredients</h4>
+        <ul>
+          {#each version.ingredients as ingredient}
+            <li>{ingredient}</li>
+          {/each}
+        </ul>
+      {/if}
+      {#if version.instructions}
+        <h4>Instructions</h4>
+        <ol>
+          {#each version.instructions as instruction}
+            <li>{instruction}</li>
+          {/each}
+        </ol>
+      {/if}
+    {/each}
   {/if}
 </div>
--- src/RecipeList.svelte
+++ src/RecipeList.svelte
@@ -2,18 +2,26 @@
 <script>
   import all_recipes from './recipes.json'
   let categories = all_recipes.map(r=>r.categories).flat().filter((v,i,a)=>a.indexOf(v)===i).filter(cat=>!!cat)
+  categories.sort((a,b)=>a>b ? 1 : -1)
   import Recipe from './Recipe.svelte'
   let search = ""
-  let selected_categories = []
+  let filtered_categories = { included: [], excluded: [] }
 
   let recipes = all_recipes
   $: {
     recipes = all_recipes.slice()
-    if (selected_categories.length > 0){
+    if (filtered_categories.included.length > 0){
       recipes = recipes.filter(r=>{
         if (!r.categories){ return false }
-        let intersection = r.categories.filter(cat => selected_categories.includes(cat))
-        return intersection.length > 0
+        let intersection = r.categories.filter(cat => filtered_categories.included.includes(cat))
+        return intersection.length >= filtered_categories.included.length
+      })
+    }
+    if (filtered_categories.excluded.length > 0){
+      recipes = recipes.filter(r=>{
+        if (!r.categories){ return true }
+        let intersection = r.categories.filter(cat => filtered_categories.excluded.includes(cat))
+        return intersection.length == 0
       })
     }
     if(search){
@@ -23,21 +31,56 @@
     }
     recipes.sort((a,b)=>a.name > b.name ? 1 : -1)
   }
+
+  const toggle_include = category => {
+    filtered_categories.excluded = filtered_categories.excluded.filter(c => c != category)
+    if (filtered_categories.included.includes(category)){ // was included, need to remove
+      filtered_categories.included = filtered_categories.included.filter(c => c != category)
+    } else { // was not included, need to add
+      filtered_categories.included = [...filtered_categories.included, category]
+    }
+  }
+  const toggle_exclude = category => {
+    filtered_categories.included = filtered_categories.included.filter(c => c != category)
+    if (filtered_categories.excluded.includes(category)){ // was excluded, need to remove
+      filtered_categories.excluded = filtered_categories.excluded.filter(c => c != category)
+    } else { // was not excluded, need to add
+      filtered_categories.excluded = [...filtered_categories.excluded, category]
+    }
+  }
 </script>
 <style>
   .category_area{ margin-bottom: 1em; }
-  .category_checkbox{ margin-right: 10pt;}
+  .category_box{ border: 1px solid white; display: inline-block; box-sizing: border-box; margin-right: 5pt; }
+  .category-name{ padding: 3pt; }
+  .category_button{ background-color: white; border-width: 0; color: #515151; position: relative; padding: 4pt 13pt; }
+  .selected{ background-color: #99FF66; }
+  .invisible{ visibility: hidden; }
+  .checkmark{ position: absolute; left: 2pt; color: black; font-weight: bold; }
 </style>
 <div>
   <div class="category_area">
-    {#each categories as category}
-      <label class="category_checkbox">
-        <input type="checkbox" bind:group={selected_categories} value={category}>
-        {category}
-      </label>
-    {/each}
+    <div>
+      {#each categories as category}
+        <div class="category_box">
+          <span class="category_element category-name">{category}</span><button
+            class="category_element category_button"
+            class:selected={filtered_categories.included.includes(category)}
+            on:click={()=>{toggle_include(category)}}>
+              <span class="checkmark" class:invisible={!filtered_categories.included.includes(category)}>✓</span>
+              In
+          </button><button
+            class="category_element category_button"
+            class:selected={filtered_categories.excluded.includes(category)}
+            on:click={()=>{toggle_exclude(category)}}>
+              <span class="checkmark" class:invisible={!filtered_categories.excluded.includes(category)}>✓</span>
+              Out
+            </button>
+        </div>
+      {/each}
+    </div>
   </div>
-  <div>Search: <input bind:value={search} /></div>
+  <div style="font-size: 18pt;">Search: <input style="font-size: 18pt" bind:value={search} /></div>
   {#if search}<div>Searching for {search}</div>{/if}
   
   {#if recipes.length > 0}
